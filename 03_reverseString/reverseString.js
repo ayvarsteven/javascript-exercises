b2dd5898d2bb60ed83f7c47f6b723d70e648f8bc
Steven Ayvar <ayvarsteven@gmail.com>
2022-07-05 21:42:21 -0500

    completes reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+function reverseString (string) {
+    const splitString = string.split('');
+    const reversed = splitString.reverse();
+    const joined = reversed.join('');
+    return joined
 };
+
+reverseString('hello')
 
 // Do not edit below this line
 module.exports = reverseString;
