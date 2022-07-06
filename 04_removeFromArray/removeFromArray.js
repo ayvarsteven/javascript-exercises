8bd93cb7e9e10c4524afbd7fdb995c920b5af395
Steven Ayvar <ayvarsteven@gmail.com>
2022-07-05 22:22:49 -0500

    completes first iteration of removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
+const removeFromArray = function([], num) {
+    for (let i = 0; i < testArray.length; i++) {
+        const element = testArray[i];
+        if (testArray[i] === num) {
+            testArray.splice(testArray[i - 1], 1);
+        }
+        console.log(testArray[i]);
+    }
 };
 
+removeFromArray([1,2,3,4], 12)
 // Do not edit below this line
 module.exports = removeFromArray;
