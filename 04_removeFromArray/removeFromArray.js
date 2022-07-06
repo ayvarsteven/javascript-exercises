26218d0110b74966227dba7cf5b20b2f77d9a2ef
Steven Ayvar <ayvarsteven@gmail.com>
2022-07-05 22:29:44 -0500

    completes removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,13 @@
-const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
-const removeFromArray = function([], num) {
-    for (let i = 0; i < testArray.length; i++) {
-        const element = testArray[i];
-        if (testArray[i] === num) {
-            testArray.splice(testArray[i - 1], 1);
+const removeFromArray = function(array, num) {
+    const arrayRemoved = array;
+    for (let i = 0; i < arrayRemoved.length; i++) {
+        if (arrayRemoved[i] === num) {
+            arrayRemoved.splice(arrayRemoved[i - 1], 1);
         }
-        console.log(testArray[i]);
     }
+    return arrayRemoved
 };
 
-removeFromArray([1,2,3,4], 12)
+removeFromArray([1,2,3,4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
