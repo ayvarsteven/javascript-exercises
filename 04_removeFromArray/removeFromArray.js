1937cbf9efd52ea456e3c52090159c75a72fd47f
Steven Ayvar <ayvarsteven@gmail.com>
2022-07-30 14:54:53 -0500

    completes exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,25 +1,13 @@
-const removeFromArray = function(array, num) {
-    const arrayRemoved = array;
-    for (let i = 0; i < arrayRemoved.length; i++) {
-        if (arrayRemoved[i] === num) {
-            arrayRemoved.splice(arrayRemoved[i - 1], 1);
-        }
+function removeFromArray(myArray, ...variousParams) {
+  for (i = myArray.length - 1; i >= 0; i--) {
+    for (parameter of variousParams) {
+      if (myArray[i] === parameter) {
+        myArray.splice(i, 1);
+      }
     }
-    return arrayRemoved
-};
-
-removeFromArray([1,2,3,4], 3)
-
-// function removeFromArray(arrayRemoved, ...itemsRemoved) {
-//   itemsRemoved = [...itemsRemoved];
-//   arrayRemoved.splice(0, itemsRemoved[0])
-//   return arrayRemoved
-// }
-
-
-// const test = removeFromArray([1, 2, 3, 4], 2, 4);
-
-// console.log(test);
+  }
+  return myArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
