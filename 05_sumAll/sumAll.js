25b564411be925fc2dff9598fbbaa6e9e30343dc
Steven Ayvar <ayvarsteven@gmail.com>
2022-07-31 16:51:25 -0500

    unlocks tests and solves for problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  for (i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] === "string") {
+      return "ERROR";
+    } else if (typeof arguments[i] === "object") {
+      return "ERROR";
+    } else if(arguments[i] < 0) {
+      return "ERROR"
+    }
+  }
+  let addedNumbers = [];
+  let sumNums = 0;
+  for (i = 0; i <= a; i++) {
+    addedNumbers.splice(i, 0, i);
+  }
+  for (i = 0; i <= b; i++) {
+    addedNumbers.splice(i, 0, i);
+  }
 
+  const reducedArray = addedNumbers.filter((c, index) => {
+    return addedNumbers.indexOf(c) === index;
+  });
+
+  console.log(reducedArray);
+  for (i = 0; i < reducedArray.length; i++) {
+    sumNums += reducedArray[i];
+  }
+
+  return sumNums;
 };
 
 // Do not edit below this line
