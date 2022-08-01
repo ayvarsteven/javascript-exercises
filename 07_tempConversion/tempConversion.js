384b36161c732c157a4ab46347c3a40a31ecc8b8
Steven Ayvar <ayvarsteven@gmail.com>
2022-08-01 16:15:50 -0500

    completes tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (farenheit) {
+  let temp = farenheit - 32;
+  temp = temp * 0.5556;
+  temp = parseFloat(temp).toFixed(1)
+  return Number(temp);
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  let temp = celsius * 1.8;
+  temp += 32;
+  temp = parseFloat(temp).toFixed(1);
+  return Number(temp);
 };
 
 // Do not edit below this line
