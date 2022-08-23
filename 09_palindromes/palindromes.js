302d0e358195e7167372bbc7d827d45d44df4e0d
Steven Ayvar <ayvarsteven@gmail.com>
2022-08-23 14:37:23 -0500

    creates basic working function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const [...stringArray] = string
+    const reversedArray = stringArray.concat().reverse();
+    for (a = 0; a < stringArray.length; a++) {
+        for(b = 0; b < reversedArray.length; b++) {
+            if(stringArray[a] != reversedArray[b]) {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
 };
+
+let test1 = palindromes('racecar')
+console.log(test1)
 
 // Do not edit below this line
 module.exports = palindromes;
