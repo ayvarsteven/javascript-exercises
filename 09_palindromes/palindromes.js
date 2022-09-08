8a803d1a1af4d2c6d1262704d2ab8cc6340fafe7
Steven Ayvar <ayvarsteven@gmail.com>
2022-09-07 20:53:31 -0500

    90% completion on palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,19 +1,19 @@
 const palindromes = function (string) {
-    const [...stringArray] = string
+    const [...stringArray] = string.toUpperCase();
     const reversedArray = stringArray.concat().reverse();
     for (a = 0; a < stringArray.length; a++) {
         for(b = 0; b < reversedArray.length; b++) {
-            if(stringArray[a] != reversedArray[b]) {
-                return false
-            } else {
+            if(stringArray[a] == reversedArray[b]) {
                 return true
+            } else {
+                return false
             }
         }
     }
 };
 
-let test1 = palindromes('racecar')
-console.log(test1)
+console.log(palindromes('a nut for a jar of tuna'))
+console.log(palindromes('rats live on no evil star'))
 
 // Do not edit below this line
 module.exports = palindromes;
