0c8ad7b63dfa4a90fb75dc040c0a8be567c2d2ae
Steven Ayvar <ayvarsteven@gmail.com>
2022-10-15 13:04:55 -0500

    completes fibonacci practice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+// const fibonacciArray = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]; 
 
-};
+const fibonacci = function(n) {
+    if(n < 0 ) return 'OOPS'
+    if(n < 3) return 1
+    let a = 1;
+    let b = 1;
+
+    for(i =2; i < n; i++) {
+        const c = a + b
+        a = b
+        b = c
+    }
+    return b
+}
+
+// console.log(fibonacci(-1)) // should return 2 
+// console.log(fibonacci(0)) // should return 3 
+// console.log(fibonacci(3)) // should return 2 
+// console.log(fibonacci(6)) // should return 8 
+// console.log(fibonacci(10)) // should return 55 
+// console.log(fibonacci(15)) // should return 610 
 
 // Do not edit below this line
 module.exports = fibonacci;
