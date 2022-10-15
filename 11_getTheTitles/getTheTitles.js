e3b24d3e42a6adb55d3dc141f3bfa6cb4f162068
Steven Ayvar <ayvarsteven@gmail.com>
2022-10-15 13:13:00 -0500

    completes get the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
 
+const getTheTitles = function(books) {
+    const result = books.map(book => book.title)
+    return result
 };
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
